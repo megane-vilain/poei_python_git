d6888bf26e2b616735788bccb94673a7a9f2dfeb
Second <megane.vilain@gmail.com>
2022-09-06 18:14:03 +0200

    Revert "add create player function" Wrong branch :/
    This reverts commit af0a139473f2bbc3752f44541f9b12618cc7ed8d.

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-def create_player(name, pv, force, armure):
-    return [name, pv, force, armure]
-
 if __name__ == '__main__':
-    player = create_player('Megane', 50, 5, 5)
-    print('Welcome to the game', player[0])    
-
+    print('Welcome to the game!')
