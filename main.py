db6cba7e14334f9f872d133cc5fd6bb7654f4682
First developer <megane.vilain@gmail.com>
2022-09-06 17:37:04 +0000

    Add function create monster and generate monster

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+from random import random
+
+
+import random
+
+def create_monster():
+    monster_name = input('Choose a name for the next opponent: ')
+    monster =  generate_monster(monster_name)
+    return monster
+
+def generate_monster(monster_name):
+    return [monster_name, random.randint(5,20), random.randint(3,8), random.randint(0,5)]
+
+
 if __name__ == '__main__':
     print('Welcome to the game!')
