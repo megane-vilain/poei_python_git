846c8353c0ae6fffcf2ae8a5ea568ee350055a46
First developer <megane.vilain@gmail.com>
2022-09-09 12:18:02 +0000

    show monster list

--- main.py
+++ main.py
@@ -14,28 +14,26 @@ def create_player(name, pv, force, armure):
 @app.route('/play_game', methods=['GET', 'POST'])
 def play_game():
     if request.method == 'POST':
+        monster_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        monster_slain = []
         nb_monsters_killed = 0
         player_name = request.form.get('player')
         player = create_player(player_name, 50, 5, 5)
         while player[1] > 0:
-            start_combat(player)
+            monster_name = random.choice(monster_names)
+            start_combat(player, monster_name)
             if(player[1] > 0):
                 nb_monsters_killed = nb_monsters_killed + 1
-        return render_template("play.html", player=player_name, nb_monster=str(nb_monsters_killed))        
+                monster_slain.append(monster_name)
+        return render_template("play.html", player=player_name, nb_monster=str(nb_monsters_killed), monster_list = monster_slain)        
     return render_template("play.html")
-
-def create_monster():
-    monster_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    monster_name = random.choice(monster_names)
-    return generate_monster(monster_name)
 
 def generate_monster(mosnster_name):
     return [mosnster_name, random.randint(5,20), random.randint(3,8), random.randint(0,5)]
 
 
-def start_combat(player):
-    monster = create_monster()
-    player
+def start_combat(player, monster_name):
+    monster = generate_monster(monster_name)
     while player[1] > 0 and monster[1] > 0:
         monster[1] = handle_damage(monster[1], monster[2], player[2])
         if(monster[1] > 0):
